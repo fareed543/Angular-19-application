0661d2becb6f2742bbdf0926f578a34335fe545c
Mohammad Fareed <fareed543@gmail.com>
2025-09-13 08:58:45 +0530

    Revert "GSPANN Technologies L1 Test"
    This reverts commit d9f8d09bc4ff99457e915f999d92e6a8bf8b8d3f.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { ProductService } from './product/product.service';
-import { Product } from './product/product.model';
-import { CommonModule } from '@angular/common';
-import { ProductComponent } from './product/product.component';
 
 @Component({
   selector: 'app-root',
-  imports: [CommonModule, ProductComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent implements OnInit{
+export class AppComponent {
   title = 'Angular';
-
-  productList !: Product[];
-  constructor(private productService :ProductService){}
-  ngOnInit(){
-    this.getProducts();
-  }
-
-  getProducts(){
-    this.productService.getProducts().subscribe((response)=>{
-      this.productList = response;
-    });
-  }
 }
